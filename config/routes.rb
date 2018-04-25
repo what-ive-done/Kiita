cad09538e5d27c3e9714beb469ff4c4c6f760d47
ChangJoo Park <pcjpcj2@gmail.com>
2018-04-25 23:52:50 +0900

    Devise와 users controller 순서 지켜야함

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :users, only: [:show] do
     resources :items
   end
 
-  devise_for :users
   get 'static_pages/home'
   get 'static_pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
