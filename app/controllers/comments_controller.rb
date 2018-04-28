11cd370163533d6488c3a89437d3e9cdb56f5370
ChangJoo Park <pcjpcj2@gmail.com>
2018-04-28 12:12:31 +0900

    #53 댓글 삭제하기

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,30 @@
 class CommentsController < ApplicationController
+  before_action :set_item, only: [:destroy]
+
   def create
     @item = Item.find(params[:item_id])
     @comment = @item.comments.create(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
-      redirect_to user_item_path(@item.user, @item)
+      redirect_to user_item_path(@item.user.username, @item)
     else
       flash.now[:danger] = "error"
     end
   end
 
+  def destroy
+    if @comment.user.id == current_user.id
+      if @comment.destroy
+        redirect_to user_item_path(@comment.user.username, @comment.item)
+      end
+    end
+  end
+
   private
+    def set_item
+      @comment = Comment.find(params[:id])
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
       params.require(:comment).permit(:body)
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :comments, only: [:destroy]
+
   get 'static_pages/home'
   get 'static_pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
